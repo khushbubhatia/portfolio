e4080a7a3008b810096bffb2902076b919313ff4
khushbubhatia <khushibhatia520@gmail.com>
2024-02-21 02:02:02 -0500

    removed boxshadow

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,9 @@ import "./Navbar.css";
 import {Link} from 'react-scroll';
 const Navbar = () => {
   return (
-        <nav className='Navbar'>
+        <nav className='Navbar' >
             <div className='desktopMenu'>
-            <Link activeClass="active" to= "Intro" spy ={true} smooth = {true} offset={-100}  duration={500} className = 'desktopMenuListItem'>Home</Link>
+            <Link activeClass="active" to= "intro" spy ={true} smooth = {true} offset={-100}  duration={500} className = 'desktopMenuListItem'>Home</Link>
             <Link  activeClass="active" to= "college" spy ={true} smooth = {true} offset={-100}  duration={500} className = 'desktopMenuListItem'> About</Link>
             <Link  activeClass="active" to= "projects" spy ={true} smooth = {true} offset={-100}  duration={500} className = 'desktopMenuListItem'>Projects</Link>
             <Link  activeClass="active" to= "contact" spy ={true} smooth = {true} offset={-100}  duration={500}  className = 'desktopMenuListItem'>Contact</Link>
