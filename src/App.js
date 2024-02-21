05d50f86396a85b5334b58326a9c803f38acc59c
khushbubhatia <khushibhatia520@gmail.com>
2024-02-21 01:32:12 -0500

    commit3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import Skills from "./components/Skills/skills";
 import College from "./components/College/college";
@@ -7,7 +6,6 @@ import Contact from "./components/Contact/contact";
 function App() {
     return(
     <div className="App">
-<Navbar />
 <Intro />
 <Skills/>
 <College />
